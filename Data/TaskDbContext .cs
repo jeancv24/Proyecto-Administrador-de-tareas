0fb3bf9daf81445e265c7651f38f5d9fa1bd23a9
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2024-09-17 16:02:20 -0600

    DB context

---
+++ Data/TaskDbContext .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Administrador_de_tareas.Models;
+using Task = Proyecto_Administrador_de_tareas.Models.Task;
+
+namespace Proyecto_Administrador_de_tareas.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=tasks.db");
+        }
+    }
+}
