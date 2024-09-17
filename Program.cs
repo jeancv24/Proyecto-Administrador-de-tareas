2d0ce57f1987cb0efbeca310ab6f89d30de466e1
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2024-09-17 16:01:12 -0600

    Inicializacion de la base de datos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Proyecto_Administrador_de_tareas.Data;
+using Proyecto_Administrador_de_tareas.Models;
+using Task = Proyecto_Administrador_de_tareas.Models.Task;
+
 namespace Proyecto_Administrador_de_tareas
 {
-    internal static class Program
+    class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -8,6 +12,15 @@ namespace Proyecto_Administrador_de_tareas
         [STAThread]
         static void Main()
         {
+            using (var db = new TaskDbContext())
+            {
+                db.Database.EnsureCreated(); // Crea la base de datos si no existe
+
+                // Insertar una tarea de ejemplo
+                db.Tasks.Add(new Task { Description = "Nueva Tarea", Status = "PENDIENTE", Priority = "ALTA", DueDate = DateTime.Now.AddDays(7), Notes = "Se debe bañar al perro" });
+                db.SaveChanges();
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
