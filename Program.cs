a240d139c6ebcdba3317e00f928f80c4ebc15cc2
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2024-09-17 22:18:20 -0600

    Simplificacion del Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Proyecto_Administrador_de_tareas.Data;
 using Proyecto_Administrador_de_tareas.Models;
+using Proyecto_Administrador_de_tareas.Views;
 using Task = Proyecto_Administrador_de_tareas.Models.Task;
 
 namespace Proyecto_Administrador_de_tareas
@@ -12,19 +13,9 @@ namespace Proyecto_Administrador_de_tareas
         [STAThread]
         static void Main()
         {
-            using (var db = new TaskDbContext())
-            {
-                db.Database.EnsureCreated(); // Crea la base de datos si no existe
-
-                // Insertar una tarea de ejemplo
-                db.Tasks.Add(new Task { Description = "Nueva Tarea", Status = "PENDIENTE", Priority = "ALTA", DueDate = DateTime.Now.AddDays(7), Notes = "Se debe bañar al perro" });
-                db.SaveChanges();
-            }
-
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm()); // Lanzar el MainForm
         }
     }
 }
