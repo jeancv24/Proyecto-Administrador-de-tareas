2effa19430519e4ec4e768d5c8922eb6e90e1e16
jeancv24 <71114289+jeancv24@users.noreply.github.com>
2024-09-17 22:17:21 -0600

    Elementos visuales

--- Views/MainForm.Designer.cs
+++ Views/MainForm.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            taskDataGridView = new DataGridView();
+            txtDescription = new TextBox();
+            cmbStatus = new ComboBox();
+            cmbPriority = new ComboBox();
+            dtpDueDate = new DateTimePicker();
+            txtNotes = new TextBox();
+            BtnAddTask = new Button();
+            BtnEditTask = new Button();
+            BtnDeleteTask = new Button();
+            ((System.ComponentModel.ISupportInitialize)taskDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // taskDataGridView
+            // 
+            taskDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            taskDataGridView.Location = new Point(409, 92);
+            taskDataGridView.Name = "taskDataGridView";
+            taskDataGridView.RowHeadersWidth = 51;
+            taskDataGridView.Size = new Size(568, 227);
+            taskDataGridView.TabIndex = 0;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(124, 52);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(125, 27);
+            txtDescription.TabIndex = 1;
+            // 
+            // cmbStatus
+            // 
+            cmbStatus.FormattingEnabled = true;
+            cmbStatus.Location = new Point(112, 103);
+            cmbStatus.Name = "cmbStatus";
+            cmbStatus.Size = new Size(151, 28);
+            cmbStatus.TabIndex = 2;
+            // 
+            // cmbPriority
+            // 
+            cmbPriority.FormattingEnabled = true;
+            cmbPriority.Location = new Point(112, 159);
+            cmbPriority.Name = "cmbPriority";
+            cmbPriority.Size = new Size(151, 28);
+            cmbPriority.TabIndex = 3;
+            // 
+            // dtpDueDate
+            // 
+            dtpDueDate.Location = new Point(72, 222);
+            dtpDueDate.MinDate = new DateTime(2024, 9, 17, 0, 0, 0, 0);
+            dtpDueDate.Name = "dtpDueDate";
+            dtpDueDate.Size = new Size(250, 27);
+            dtpDueDate.TabIndex = 4;
+            // 
+            // txtNotes
+            // 
+            txtNotes.Location = new Point(124, 282);
+            txtNotes.Name = "txtNotes";
+            txtNotes.Size = new Size(125, 27);
+            txtNotes.TabIndex = 5;
+            // 
+            // BtnAddTask
+            // 
+            BtnAddTask.Location = new Point(99, 372);
+            BtnAddTask.Name = "BtnAddTask";
+            BtnAddTask.Size = new Size(94, 29);
+            BtnAddTask.TabIndex = 6;
+            BtnAddTask.Text = "Agregar";
+            BtnAddTask.UseVisualStyleBackColor = true;
+            BtnAddTask.Click += BtnAddTask_Click;
+            // 
+            // BtnEditTask
+            // 
+            BtnEditTask.Location = new Point(262, 372);
+            BtnEditTask.Name = "BtnEditTask";
+            BtnEditTask.Size = new Size(94, 29);
+            BtnEditTask.TabIndex = 7;
+            BtnEditTask.Text = "Editar";
+            BtnEditTask.UseVisualStyleBackColor = true;
+            BtnEditTask.Click += BtnEditTask_Click;
+            // 
+            // BtnDeleteTask
+            // 
+            BtnDeleteTask.Location = new Point(409, 372);
+            BtnDeleteTask.Name = "BtnDeleteTask";
+            BtnDeleteTask.Size = new Size(94, 29);
+            BtnDeleteTask.TabIndex = 8;
+            BtnDeleteTask.Text = "Eliminar";
+            BtnDeleteTask.UseVisualStyleBackColor = true;
+            BtnDeleteTask.Click += BtnDeleteTask_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1027, 554);
+            Controls.Add(BtnDeleteTask);
+            Controls.Add(BtnEditTask);
+            Controls.Add(BtnAddTask);
+            Controls.Add(txtNotes);
+            Controls.Add(dtpDueDate);
+            Controls.Add(cmbPriority);
+            Controls.Add(cmbStatus);
+            Controls.Add(txtDescription);
+            Controls.Add(taskDataGridView);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)taskDataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView taskDataGridView;
+        private TextBox txtDescription;
+        private ComboBox cmbStatus;
+        private ComboBox cmbPriority;
+        private DateTimePicker dtpDueDate;
+        private TextBox txtNotes;
+        private Button BtnAddTask;
+        private Button BtnEditTask;
+        private Button BtnDeleteTask;
     }
 }
